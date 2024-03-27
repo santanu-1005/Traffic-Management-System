ccb9d4894a3f52a20ba9042ad4a20966c442c9f2
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2024-03-27 20:41:10 +0530

    removed 'main' function, set frame width to 1080

--- model_files/main.py
+++ model_files/main.py
@@ -4,6 +4,9 @@ import numpy as np
 import supervision as sv
 from ultralytics import YOLO
 
+# Video output codec
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+print(fourcc)
 ZONE_POLYGON = np.array([
     [0,0],
     [900,0],
@@ -15,43 +18,48 @@ def parse_arg() -> argparse.Namespace:
     parser = argparse.ArgumentParser(description="YOLOv8 Live")
     parser.add_argument(
         "--webcam-resolution",
-        default=[1280, 720],
+        default=[1080, 720],
         nargs=2, 
         type=int
     )
     args = parser.parse_args()
     return args
 
-def main():
-    args = parse_arg()
-    frame_width, frame_height = args.webcam_resolution
-     
-    cap = cv2.VideoCapture("test.mp4")
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+args = parse_arg()
+frame_width, frame_height = args.webcam_resolution
+    
+# cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("test.mp4")
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
+out = cv2.VideoWriter('result.mp4', fourcc, 25.0, (frame_width, frame_height))
 
-    model = YOLO("yolov8l.pt")
-    box_annotator = sv.BoxAnnotator(
-        thickness = 2,
-        text_thickness = 2,
-        text_scale = 1
-    )
+model = YOLO("model_files/yolov8n.pt")
+box_annotator = sv.BoxAnnotator(
+    thickness = 2,
+    text_thickness = 2,
+    text_scale = 1
+)
 
-    zone = sv.PolygonZone(polygon = ZONE_POLYGON, frame_resolution_wh = tuple(args.webcam_resolution))
-    zone_annotator = sv.PolygonZoneAnnotator(zone = zone, color = sv.Color.red())
+zone = sv.PolygonZone(polygon = ZONE_POLYGON, frame_resolution_wh = tuple(args.webcam_resolution))
+zone_annotator = sv.PolygonZoneAnnotator(zone = zone, color = sv.Color.RED)
 
-    while True:
-        ret, frame = cap.read()
-        result = model(frame)[0]
-        detections = sv.Detections.from_ultralytics(result)  
+while True:
+    ret, frame = cap.read()
+    if not ret: break
+    result = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(result)  
 
-        frame = box_annotator.annotate(scene = frame, detections = detections) 
-        zone.trigger(detections = detections)
-        frame = zone_annotator.annotate(scene = frame)
-        cv2.imshow("yolov8",frame)
+    frame = box_annotator.annotate(scene = frame, detections = detections) 
+    zone.trigger(detections = detections)
+    frame = zone_annotator.annotate(scene = frame)
+    out.write(frame)
+    cv2.imshow("yolov8",frame)
 
-        if (cv2.waitKey(30) == 27):
-            break
-
-if __name__ == '__main__':
-    main()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# Release resources
+cap.release()
+out.release()
+cv2.destroyAllWindows()
