356931b5b36a3c7d7ed2c76ff09a48162a45f22b
Aditya Kumar Sahu <adityasahu1124@gmail.com>
2024-03-31 12:31:55 +0530

    included object tracking

--- model_files/main.py
+++ model_files/main.py
@@ -3,9 +3,13 @@ import argparse
 import numpy as np
 import supervision as sv
 from ultralytics import YOLO
+from ultralytics.solutions.object_counter import ObjectCounter
+
+counter = ObjectCounter()
+counter.set_args({0:'Ambulance', 5:'Bus', 2:'Car', 7:'Truck'}, [(0,0), (0,720), (720, 720), (720,0)])
 
 # Video output codec
-fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 print(fourcc)
 ZONE_POLYGON = np.array([
     [0,0],
@@ -32,7 +36,7 @@ frame_width, frame_height = args.webcam_resolution
 cap = cv2.VideoCapture("test.mp4")
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
-out = cv2.VideoWriter('result.mp4', fourcc, 25.0, (frame_width, frame_height))
+out = cv2.VideoWriter('result.avi', fourcc, 25.0, (frame_width, frame_height))
 
 model = YOLO("model_files/yolov8n.pt")
 box_annotator = sv.BoxAnnotator(
@@ -47,14 +51,23 @@ zone_annotator = sv.PolygonZoneAnnotator(zone = zone, color = sv.Color.RED)
 while True:
     ret, frame = cap.read()
     if not ret: break
-    result = model(frame)[0]
-    detections = sv.Detections.from_ultralytics(result)  
+    # result = model(frame)[0]
+    # detections = sv.Detections.from_ultralytics(result)  
 
-    frame = box_annotator.annotate(scene = frame, detections = detections) 
-    zone.trigger(detections = detections)
-    frame = zone_annotator.annotate(scene = frame)
-    out.write(frame)
-    cv2.imshow("yolov8",frame)
+    # frame = box_annotator.annotate(scene = frame, detections = detections) 
+    # zone.trigger(detections = detections)
+    # frame = zone_annotator.annotate(scene = frame)
+    # out.write(frame)
+    # cv2.imshow("yolov8",frame)
+
+    result = model.track(frame, persist=True)
+    annoted_frame = result[0].plot()
+
+    count_result = counter.start_counting(frame, result)
+
+    # print(annoted_frame)
+    out.write(count_result)
+    cv2.imshow("Tracking", count_result)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
